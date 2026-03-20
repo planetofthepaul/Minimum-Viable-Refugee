d2a3e2d2f215967d59e214605de44642932f759c
planetofthepaul <planetofthepaul@gmail.com>
2026-03-19 21:48:43 -0400

    Add sitemap integration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://minimumviableexpat.com',
   output: 'static',
   build: {
     format: 'directory'
-  }
+  },
+  integrations: [sitemap()]
 });
