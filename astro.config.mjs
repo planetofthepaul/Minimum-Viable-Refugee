298bfd8ef5e703825ec85899bb18238ee50441de
planetofthepaul <planetofthepaul@gmail.com>
2026-03-19 21:51:31 -0400

    Remove sitemap integration from astro.config.mjs
    Removed sitemap integration from Astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-
 export default defineConfig({
   site: 'https://minimumviableexpat.com',
   output: 'static',
   build: {
     format: 'directory'
-  },
-  integrations: [sitemap()]
+  }
 });
